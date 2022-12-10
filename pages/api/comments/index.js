cefa51ffe88ec8546a4430cf11652c025b47fc70
shourovahmd <moh.abuhanifa@gmail.com>
2022-12-11 04:47:30 +0600

    imported files

---
+++ pages/api/comments/index.js
@@ -0,0 +1,17 @@
+import { comments } from "../../../data/comments";
+
+export default function handler(req, res) {
+  
+  if (req.method === "GET") {
+    res.status(200).json(comments);
+  }
+   else if (req.method === "POST") {
+    const comment = req.body.comments;
+    const newComment = {
+      id: comments.length + 1,
+      text: comment,
+    };
+    comments.push(newComment);
+    res.status(201).json(newComment);
+  }
+}
---
+++ pages/comments/index.js
@@ -0,0 +1,43 @@
+import React, { useState } from "react";
+
+function CommentsPage() {
+  const [comments, setComments] = useState([]);
+  const [comment, setComment] = useState("");
+  const fetchComments = async () => {
+    const res = await fetch("http://localhost:3000/api/comments");
+    const data = await res.json();
+    setComments(data);
+  };
+
+  const submitComment = async () => {
+    const res = await fetch("http://localhost:3000/api/comments", {
+      method: "POST",
+      body: JSON.stringify({ comment }),
+      headers: { "Content-Type": "application/json" },
+    });
+    const data = await res.json();
+    console.log(data);
+  };
+
+  return (
+    <div className="bg-white text-gray-700 m-20">
+      <input
+        type="text"
+        value={comment}
+        onChange={(e) => setComment(e.target.value)}
+        className="bg-white text-gray-700 m-2"
+      />
+      <button onClick={submitComment}>Submit</button>
+      <br />
+      <button onClick={fetchComments}>Load Comments</button>
+      {comments &&
+        comments.map((c) => (
+          <div key={c.id}>
+            <h2>{c.text}</h2>
+          </div>
+        ))}
+    </div>
+  );
+}
+
+export default CommentsPage;
