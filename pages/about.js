2e76ba55b3727d90f5679314863a8e6f244bfb92
shourovahmd <moh.abuhanifa@gmail.com>
2022-12-12 03:55:44 +0600

    images

--- pages/about.js
+++ pages/about.js
@@ -1,9 +1,15 @@
+import Image from "next/image";
 import React from "react";
 
 export default function About() {
   return (
     <div>
       <h3>About Page</h3>
+      {[1, 2, 3, 4, 5, 6].map((i) => (
+        <div key={i}>
+          <Image src={`./${i}.avif`} alt="img" width="280" height="400" />
+        </div>
+      ))}
     </div>
   );
 }
